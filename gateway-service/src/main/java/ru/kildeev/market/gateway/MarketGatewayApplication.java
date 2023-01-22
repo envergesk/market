4437b092dfba81b58997ff06a36427694b0bd503
enverge <h4tesk@gmail.com>
2023-01-22 08:39:15 +0300

    auth-service v1 added, other services completed

---
+++ gateway-service/src/main/java/ru/kildeev/market/gateway/MarketGatewayApplication.java
@@ -0,0 +1,13 @@
+package ru.kildeev.market.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketGatewayApplication.class, args);
+    }
+
+}
