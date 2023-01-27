5f8dd69e104a184b89a2588c22326578fab4c60c
envergesk <ru.certificate.plus@gmail.com>
2023-01-27 09:26:56 +0300

    auth-service added

--- core-service/src/main/resources/db/migration/V1__init.sql
+++ core-service/src/main/resources/db/migration/V1__init.sql
@@ -24,40 +24,6 @@ values ('Молоко', 100.20, 1),
        ('Сыр', 90.20, 1),
        ('Масло', 320.00, 1);
 
-create table users
-(
-    id          bigserial primary key,
-    username    varchar(36) not null,
-    password    varchar(80) not null,
-    created_at  timestamp default current_timestamp,
-    updated_at  timestamp default current_timestamp
-);
-
-create table roles
-(
-    id          bigserial primary key,
-    name        varchar(36) not null
-);
-
-create table users_roles
-(
-    user_id     bigint not null references users(id),
-    role_id     bigint not null references roles(id),
-    primary key (user_id, role_id)
-);
-
-insert into roles(name)
-values ('ROLE_USER'),
-       ('ROLE_ADMIN');
-
-insert into users (username, password)
-values ('bob', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i'),
-       ('jack', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i');
-
-insert into users_roles(user_id, role_id)
-values (1, 1),
-       (2, 2);
-
 create table orders
 (
     id          bigserial primary key,
